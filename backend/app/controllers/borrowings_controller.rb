d75d1adaf5618e15bb0232ed09dcd263f4e846aa
Danilo Cândido <danilod07@gmail.com>
2025-07-27 18:43:01 -0300

    Remove unecessary code

--- backend/app/controllers/borrowings_controller.rb
+++ backend/app/controllers/borrowings_controller.rb
@@ -1,16 +1,5 @@
 class BorrowingsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_borrowing, only: %i[ show update destroy ]
-
-  def index
-    @borrowings = Borrowing.accessible_by(current_ability)
-
-    render json: @borrowings
-  end
-
-  def show
-    render json: @borrowing
-  end
 
   def create
     @borrowing = current_user.borrowings.new(borrowing_params)
@@ -32,23 +21,7 @@ class BorrowingsController < ApplicationController
     render json: @borrowing, status: :not_found
   end
 
-  def update
-    if @borrowing.update(borrowing_params)
-      render json: @borrowing
-    else
-      render json: @borrowing.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @borrowing.destroy!
-  end
-
   private
-
-  def set_borrowing
-    @borrowing = Borrowing.find(params.expect(:id))
-  end
 
   def borrowing_params
     params.expect(borrowing: %i[user_id book_id borrowed_at due_at returned_at ])
